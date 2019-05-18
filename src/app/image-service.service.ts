6eff5d02a6980c91264acb24f3fa201437e63342
Oleg Mazhanov <oleg.mazhanov@gmail.com>
2019-05-18 18:06:04 +1200

    ImageService simplified

--- src/app/image-service.service.ts
+++ src/app/image-service.service.ts
@@ -5,36 +5,17 @@ import {Injectable} from '@angular/core';
 })
 export class ImageServiceService {
 
-  myImages2 = {
-    'castle': ['../assets/castle/img1.jpg', '../assets/castle/img2.jpg', '../assets/castle/img3.jpg', '../assets/castle/img4.jpg',
-      '../assets/castle/img5.jpg', '../assets/castle/img6.jpg', '../assets/castle/img7.jpg', '../assets/castle/img8.jpg', '../assets/castle/img9.jpg',
-      '../assets/castle/img10.jpg', '../assets/castle/img11.jpg', '../assets/castle/img12.jpg', '../assets/castle/img13.jpg', '../assets/castle/img14.jpg'
-      , '../assets/castle/img15.jpg', '../assets/castle/img16.jpg', '../assets/castle/img17.jpg', '../assets/castle/img18.jpg', '../assets/castle/img19.jpg'
-      , '../assets/castle/img20.jpg', '../assets/castle/img21.jpg', '../assets/castle/img22.jpg', '../assets/castle/img23.jpg', '../assets/castle/img24.jpg'
-      , '../assets/castle/img25.jpg', '../assets/castle/img26.jpg', '../assets/castle/img27.jpg', '../assets/castle/img28.jpg', '../assets/castle/img29.jpg'
-      , '../assets/castle/img30.jpg', '../assets/castle/img31.jpg', '../assets/castle/img32.jpg', '../assets/castle/img33.jpg', '../assets/castle/img34.jpg'],
-      'paris': ['../assets/paris/1.jpg', '../assets/paris/2.jpg', '../assets/paris/3.jpg', '../assets/paris/4.jpg', '../assets/paris/5.jpg',
-      '../assets/paris/6.jpg', '../assets/paris/7.jpg', '../assets/paris/8.jpg', '../assets/paris/9.jpg', '../assets/paris/10.jpg', '../assets/paris/11.jpg',
-        '../assets/paris/12.jpg']
-  };
-
-  myImages: String[] = ['../assets/castle/img1.jpg', '../assets/castle/img2.jpg', '../assets/castle/img3.jpg', '../assets/castle/img4.jpg',
-    '../assets/castle/img5.jpg', '../assets/castle/img6.jpg', '../assets/castle/img7.jpg', '../assets/castle/img8.jpg', '../assets/castle/img9.jpg'
-    , '../assets/castle/img10.jpg', '../assets/castle/img11.jpg', '../assets/castle/img12.jpg', '../assets/castle/img13.jpg', '../assets/castle/img14.jpg'
-    , '../assets/castle/img15.jpg', '../assets/castle/img16.jpg', '../assets/castle/img17.jpg', '../assets/castle/img18.jpg', '../assets/castle/img19.jpg'
-    , '../assets/castle/img20.jpg', '../assets/castle/img21.jpg', '../assets/castle/img22.jpg', '../assets/castle/img23.jpg', '../assets/castle/img24.jpg'
-    , '../assets/castle/img25.jpg', '../assets/castle/img26.jpg', '../assets/castle/img27.jpg', '../assets/castle/img28.jpg', '../assets/castle/img29.jpg'
-    , '../assets/castle/img30.jpg', '../assets/castle/img31.jpg', '../assets/castle/img32.jpg', '../assets/castle/img33.jpg', '../assets/castle/img34.jpg'];
+  private themes = {'castle': '34', 'paris': '81', 'spain': '163'};
 
   constructor() {
   }
 
   getImages(id: string) {
-    // return this.myImages.slice(0);
-    return this.myImages2[id];
+    const a1: String[] = [];
+    /* return this.myImages.slice(0);*/
+    for (let i = 1; i <= this.themes[id]; i++) {
+      a1.push('../assets/' + id + '/' + i.toString() + '.jpg');
+    }
+    return a1;
   }
-
-  /*getImage(id: number) {
-    return this.getImages()[id];
-  }*/
 }
